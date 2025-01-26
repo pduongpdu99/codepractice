65a78ae60ed0ae0c892a58e5942ac97a4b646158
Phạm Dương <pduongpdu99@gmail.com>
2025-01-26 15:10:34 +0700

    first commit

---
+++ LeetCode/TwoSum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        res = []
+        for i in range(0, len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    res.append(i)
+                    res.append(j)
+                    break
+        return res
---
+++ main.py
@@ -0,0 +1,7 @@
+from LeetCode.TwoSum import Solution as TwoSumSolution
+
+if __name__ == "__main__":
+    nums = [2, 7, 11, 15]
+    target = 9
+    res = TwoSumSolution().twoSum(nums, target)
+    print(res)
